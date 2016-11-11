3b8029d8effe69477c22787a203695ce57c39133
RPalotra <rakesh.palotra@gmail.com>
2016-11-11 18:34:45 +0530

    Add files via upload

---
+++ CloudKit/CloudKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudKit
+//
+//  Created by rakesh palotra on 29/04/16.
+//  Copyright © 2016 webdunia pvt ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
