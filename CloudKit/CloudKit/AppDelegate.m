ddab36c63362645276b311a85a4ea0ed37aee34b
RakeshPalotra <rakesh.palotra@gmail.com>
2016-11-11 19:03:12 +0530

    changes in app delegate

--- CloudKit/CloudKit/AppDelegate.m
+++ CloudKit/CloudKit/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    // Test by narendra
+    // Test by narendra and rakesh
     return YES;
 }
 
