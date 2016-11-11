93f6abd90c1dcfe020ba6ea63b06c0ada1763b18
RakeshPalotra <rakesh.palotra@gmail.com>
2016-11-11 18:40:08 +0530

    Test by rakesh

--- CloudKit/CloudKit/AppDelegate.m
+++ CloudKit/CloudKit/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // Test by rakesh
     return YES;
 }
 
